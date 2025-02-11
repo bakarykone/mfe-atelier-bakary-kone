5cea83c9087851babd37b37c947f384281dc28f2
bakarykone <bakarykone729@gmail.com>
2025-02-11 14:32:40 +0100

    Ajout de la prise en charge du module fédéré pour le composant Header et mise à jour de la configuration du projet

--- shell/rsbuild.config.ts
+++ shell/rsbuild.config.ts
@@ -1,6 +1,19 @@
-import { defineConfig } from '@rsbuild/core';
-import { pluginReact } from '@rsbuild/plugin-react';
+import { defineConfig } from "@rsbuild/core";
+import { pluginReact } from "@rsbuild/plugin-react";
+import { pluginModuleFederation } from "@module-federation/rsbuild-plugin";
 
 export default defineConfig({
-  plugins: [pluginReact()],
+  plugins: [
+    pluginReact(),
+    pluginModuleFederation({
+      name: "shell",
+      remotes: {
+        header_mfe: "header_mfe@http://localhost:3000/mf-manifest.json",
+      },
+      shared: ["react", "react-dom"],
+    }),
+  ],
+  server: {
+    port: 2000,
+  },
 });
--- shell/src/App.tsx
+++ shell/src/App.tsx
@@ -1,10 +1,13 @@
 import './App.css';
+import Header from 'header_mfe/header';
+
 
 const App = () => {
   return (
     <div className="content">
-      <h1>Rsbuild with React</h1>
-      <p>Start building amazing things with Rsbuild.</p>
+      <p>Import du Header MFE</p>
+      <Header />
+      <h1>Shell APP</h1>
     </div>
   );
 };
