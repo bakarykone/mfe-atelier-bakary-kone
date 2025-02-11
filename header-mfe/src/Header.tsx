32532d962036135804d357ce7313f33837fbe99d
bakarykone <bakarykone729@gmail.com>
2025-02-11 14:15:40 +0100

    Ajout de la structure de base pour le projet header-mfe avec des composants, des styles et une configuration de build.

---
+++ header-mfe/src/Header.tsx
@@ -0,0 +1,3 @@
+export default function Header() {
+    return <div>Header Component in header-mte</div>;
+  }
--- shell/src/index.tsx
+++ shell/src/index.tsx
@@ -1,12 +1,13 @@
-import('./bootstrap');
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root')!);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+const rootEl = document.getElementById('root');
+if (rootEl) {
+  const root = ReactDOM.createRoot(rootEl);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  );
+}
